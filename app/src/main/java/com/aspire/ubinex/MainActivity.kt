5b02c05b78acede68017b26195ff6a912b6acb12
Aspire0071400 <prabhujivats@gmail.com>
2024-03-28 15:05:48 +0530

    viewPager added and synced with bottom navigation, design not refined.

--- app/src/main/java/com/aspire/ubinex/MainActivity.kt
+++ app/src/main/java/com/aspire/ubinex/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.navigation.NavController
+import androidx.viewpager.widget.ViewPager
 import com.aspire.ubinex.databinding.ActivityMainBinding
 import com.aspire.ubinex.fragments.AccountFragment
 import com.aspire.ubinex.fragments.MessageFragment
@@ -26,27 +29,57 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val pagerAdapter = MyPagerAdapter(supportFragmentManager)
+        binding.viewPager.adapter = pagerAdapter
 
-        replaceFragment(MessageFragment())
+        //replaceFragment(MessageFragment())
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId){
-                R.id.message -> replaceFragment(MessageFragment())
-                R.id.music -> replaceFragment(MusicFragment())
-                R.id.account -> replaceFragment(AccountFragment())
+                R.id.message -> binding.viewPager.currentItem = 0
+                R.id.music -> binding.viewPager.currentItem = 1
+                R.id.account -> binding.viewPager.currentItem = 2
                 else ->{}
             }
             true
         }
 
+        binding.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            override fun onPageSelected(position: Int) {
+                binding.bottomNavigationView.selectedItemId = when (position) {
+                    0 -> R.id.message
+                    1 -> R.id.music
+                    2 -> R.id.account
+                    else -> return
+                }
+            }
+        })
+
 
 
     }
 
-    private fun replaceFragment(fragment : Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame,fragment)
-        fragmentTransaction.commit()
+//    private fun replaceFragment(fragment : Fragment){
+//        val fragmentManager = supportFragmentManager
+//        val fragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.frame,fragment)
+//        fragmentTransaction.commit()
+//    }
+
+    private inner class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+        override fun getCount(): Int = 3
+
+        override fun getItem(position: Int): Fragment {
+            return when (position) {
+                0 -> MessageFragment()
+                1 -> MusicFragment()
+                2 -> AccountFragment()
+                else -> throw IllegalArgumentException("Invalid position")
+            }
+        }
     }
 
 }
