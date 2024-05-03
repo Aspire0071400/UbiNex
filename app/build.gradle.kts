5d62b946baaaed2e555cdac80be68ca906650d0a
Aspire0071400 <prabhujivats@gmail.com>
2024-05-04 01:20:34 +0530

    camera permission added, not the camera accessibility.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,14 +55,16 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation("com.jsibbold:zoomage:1.3.1")
-    // CameraX core library
-    implementation("androidx.camera:camera-core:1.1.0-alpha09")
-    // CameraX Camera2 extension library
-    implementation("androidx.camera:camera-camera2:1.1.0-alpha09")
-    // CameraX Lifecycle library
-    implementation("androidx.camera:camera-lifecycle:1.1.0-alpha09")
-    // CameraX View library
-    implementation("androidx.camera:camera-view:1.0.0-alpha28")
+
+    val camerax_version = "1.4.0-alpha05"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    // If you want to additionally use the CameraX Lifecycle library
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    // If you want to additionally use the CameraX VideoCapture library
+    //implementation("androidx.camera:camera-video:${camerax_version}")
+    // If you want to additionally use the CameraX View class
+    implementation("androidx.camera:camera-view:${camerax_version}")
 
 
     testImplementation(libs.junit)
--- app/src/main/java/com/aspire/ubinex/SetupPage.kt
+++ app/src/main/java/com/aspire/ubinex/SetupPage.kt
@@ -13,7 +13,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.aspire.ubinex.databinding.ActivitySetupPageBinding
 import com.aspire.ubinex.model.UserDataModel
-import com.aspire.ubinex.utils.PermissionHandler
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -68,12 +67,12 @@ class SetupPage : AppCompatActivity() {
         getExistingUserData()
 
         binding.setupImage.setOnClickListener {
-
-            if(PermissionHandler.hasCameraPermission(this)){
-                openGallery()
-            }else{
-                PermissionHandler.requestCameraPermission(this)
-            }
+        openGallery()
+//            if(PermissionHandler.hasCameraPermission(this)){
+//                openGallery()
+//            }else{
+//                PermissionHandler.requestCameraPermission(this)
+//            }
         }
 
         binding.saveSetupProfile.setOnClickListener {
@@ -198,19 +197,19 @@ class SetupPage : AppCompatActivity() {
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        PermissionHandler.handlePermissionsResult(requestCode, grantResults,
-            onSuccess = {
-                // Permission granted, proceed with gallery opening
-                openGallery()
-            },
-            onFailure = {
-                // Permission denied
-                Toast.makeText(this, "Camera permission is required to select an image.", Toast.LENGTH_SHORT).show()
-            }
-        )
-    }
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        PermissionHandler.handlePermissionsResult(requestCode, grantResults,
+//            onSuccess = {
+//                // Permission granted, proceed with gallery opening
+//                openGallery()
+//            },
+//            onFailure = {
+//                // Permission denied
+//                Toast.makeText(this, "Camera permission is required to select an image.", Toast.LENGTH_SHORT).show()
+//            }
+//        )
+//    }
 
     private fun openGallery() {
         val intent = Intent()
